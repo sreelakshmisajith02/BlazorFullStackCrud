c08aa824c23059aaa78cdd151ffb6057b58c8b34
RSI <RSI@RSI-LT-34901>
2022-05-18 17:57:18 +0530

    Add project files.

---
+++ BlazorFullStackCrud/Shared/SuperHero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFullStackCrud.Shared
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string HeroName { get; set; } = String.Empty;
+
+        public Comic? Comic { get; set; }
+
+        public int ComicId { get; set; }
+    }
+}
